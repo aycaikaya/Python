5b16ee40bb7eecf5b78f26ca9e4f2e8b3448b31f
Ayça İdil Kaya <42469050+aycaikaya@users.noreply.github.com>
2022-11-07 21:34:55 +0300

    Delete m3.py

--- m3.py
+++
@@ -1,6 +0,0 @@
-class InventoryProduct(object):
-    def __init__(self,name,price,amount):
-        self.name=str(name)
-        self.price=float(price)
-        self.amount=float(amount)
-product_typed=InventoryProduct()
